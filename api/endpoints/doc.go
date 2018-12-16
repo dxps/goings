18c7b13ecd0ee54d89c166f920c00b87a6091cbc
vision8tech <vision8r@gmail.com>
2018-12-16 23:36:12 +0200

    cosmetics

---
+++ api/endpoints/doc.go
@@ -0,0 +1,6 @@
+/*
+
+This package stores all the handlers routes for the API endpoints.
+
+*/
+package endpoints
--- notes.md
+++
@@ -1,5 +0,0 @@
-
-### SQLite - dev notes
-
-Installed go-sqlite3 using `go get -u github.com/mattn/go-sqlite3`
-
