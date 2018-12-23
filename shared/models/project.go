b711c51838db28ea561b845efadd5de091bd734e
vision8tech <vision8r@gmail.com>
2018-12-23 22:14:51 +0200

    project model json fields

--- shared/models/project.go
+++ shared/models/project.go
@@ -6,12 +6,12 @@ import (
 
 // Project is the model of a project.
 type Project struct {
-	ID          string
-	Title       string
-	Description string
-	ImageURI    string
-	StartTime   time.Time
-	State       string
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	ImageURI    string    `json:"image_uri"`
+	StartTime   time.Time `json:"start_time"`
+	State       string    `json:"state"`
 }
 
 // Projects is a slice of 0-N references to Project objects.
